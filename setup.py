56ca9e3467bf1f31d9b3bcf461e930ab0d698cde
Marco Galatola <marco.galatola@linksfoundation.com>
2022-07-21 14:10:15 +0000

    Removed poetry

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name = "media_source_adaptor",
+    version = "0.0.1",
+    author = "Links Foundation",
+    description = ("Video and strean media source adaptor"),
+    url = "https://bitbucket.org/mobilesolutionsismb/media-source-adaptor/src/master/"
+)
