a6c433321f0ca5e324884447225453dc3b60ce43
Marco Galatola <marco.galatola@linksfoundation.com>
2022-08-12 13:34:38 +0000

    modify setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ setup(
     name = "media_source_adaptor",
     version = "0.0.1",
     author = "Links Foundation",
-    description = ("Video and strean media source adaptor"),
-    url = "https://bitbucket.org/mobilesolutionsismb/media-source-adaptor/src/master/"
+    description = ("Video and stream media source adaptor"),
+    url = "https://bitbucket.org/mobilesolutionsismb/media-source-adaptor/src/master/",
+    packages=['media_source_adaptor']
 )
